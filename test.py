ca9bde48c357e59fc5a31dad9ddeef6b2dc30b41
Richard Stokes <richardstokes77@gmail.com>
2024-02-17 18:13:31 -0500

    Add test for all possible winning conditions

--- test.py
+++ test.py
@@ -18,9 +18,11 @@ class TestTicTacToeGame(unittest.TestCase):
     def test__str__(self):
         x = str(self.game)
         initial_state = """
--   -   -
--   -   -
--   -   -
+			- | - | -
+			-+--+--+-
+			- | - | -
+			-+--+--+-
+			- | - | -
 """
         self.assertMultiLineEqual(initial_state, x, "The board hasn't printed correctly.")
     
@@ -35,12 +37,26 @@ class TestTicTacToeGame(unittest.TestCase):
         self.assertEqual(adjacent_coords, self.game.adjacent_coordinates(*corner_coords).sort())
 
     def test_winning_condition(self):
-        winning_coords = [(0,0), (1,1), (2,2)]
+        
+        winning_coords = {
+            ((0,0), (1,1), (2,2)),  # Diagonal
+            ((0,2), (1,1), (2,0)),  # Diagonal
+            ((0,0), (0,1), (0,2)),  # Top row
+            ((1,0), (1,1), (1,2)),  # Middle row
+            ((2,0), (2,1), (2,2)),  # Bottom row
+            ((0,0), (1,0), (2,0)),  # Left column
+            ((0,1), (1,1), (2,1)),  # Middle column
+            ((0,2), (1,2), (2,2))   # Right column
+        }
+
+
         for coords in winning_coords:
-            x,y = coords
-            self.game.board[x][y] = "X"
-        winning = self.game.winning_condition(2,2, "X")
-        self.assertTrue(winning)
+            for c in coords:
+                self.game.mark_square(*c, "X")
+            last_square = coords[-1] 
+            winning = self.game.winning_condition(*last_square, "X")
+            self.assertTrue(winning)
+            self.game.board = self.game.getCleanBoard()
 
         not_winning_coords = [(0,0), (1,0), (1,1)]
         for coords in not_winning_coords:
